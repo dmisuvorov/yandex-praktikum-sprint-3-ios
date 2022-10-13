06c8e5416a66edd38c42a1a9c270ad8cf32be7f3
Dmitry Suvorov <dmitrysuvorov9@gmail.com>
2022-10-13 23:49:31 +0300

    Бизнес логика: связка UI элементов с ViewController и обработчик нажатия на кнопку

--- yandex-praktikum-sprint-3-ios/ViewController.swift
+++ yandex-praktikum-sprint-3-ios/ViewController.swift
@@ -8,12 +8,21 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    private var counter = 0
+    
+    @IBOutlet weak var label: UILabel!
+    
+    @IBOutlet weak var button: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        label.text = "\(counter)"
     }
 
-
+    @IBAction func buttonDidTap(_ sender: Any) {
+        counter+=1
+        label.text = "\(counter)"
+    }
 }
 
