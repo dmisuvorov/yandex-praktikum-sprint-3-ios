dffff7e3e2376be4ef342965ffa475bc95af857c
Dmitry Suvorov <dmitrysuvorov9@gmail.com>
2022-10-13 23:12:32 +0300

    Initial Commit

---
+++ yandex-praktikum-sprint-3-ios/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  yandex-praktikum-sprint-3-ios
+//
+//  Created by Суворов Дмитрий Владимирович on 13.10.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
